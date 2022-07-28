19f20ba053b95f046acf1a70bf024df6db22842b
Carlos <carlos@clovrlabs.com>
2022-07-28 15:21:51 +0200

    Changed page restrictions for Channels and Nodes. Also minor improvements in view Nodes

---
+++ src/Services/ClipboardService.cs
@@ -0,0 +1,24 @@
+namespace FundsManager.Services;
+
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+public sealed class ClipboardService
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public ClipboardService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public ValueTask<string> ReadTextAsync()
+    {
+        return _jsRuntime.InvokeAsync<string>("navigator.clipboard.readText");
+    }
+
+    public ValueTask WriteTextAsync(string text)
+    {
+        return _jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+    }
+}
