6cbf9cc029722d21ec08cd0b9b81f0133654f148
Marcos <marcos@clovrlabs.com>
2022-11-11 19:29:16 +0100

    Name change for 2FA

--- src/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ src/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -181,7 +181,7 @@ namespace FundsManager.Areas.Identity.Pages.Account.Manage
             return string.Format(
                 CultureInfo.InvariantCulture,
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Microsoft.AspNetCore.Identity.UI"),
+                _urlEncoder.Encode("NodeGuard"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
