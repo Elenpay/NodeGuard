845e3225c0f9e70bda3f7703433e6d32be9718fd
Marcos <marcos@clovrlabs.com>
2023-01-09 16:43:50 +0100

    Adding Max to current validator and removing the old unused validator

--- src/Helpers/ValidationHelper.cs
+++ src/Helpers/ValidationHelper.cs
@@ -58,13 +58,21 @@ public static class ValidationHelper
 
         obj.Status = ValidationStatus.Success;
 
-        var environmentVariable = Environment.GetEnvironmentVariable("MINIMUM_WITHDRAWAL_BTC_AMOUNT") ?? throw new InvalidOperationException();
-
-        var minimum = decimal.Parse(environmentVariable, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
+        var environmentVariableMin = Environment.GetEnvironmentVariable("MINIMUM_WITHDRAWAL_BTC_AMOUNT") ?? throw new InvalidOperationException();
+        var environmentVariableMax = Environment.GetEnvironmentVariable("MAXIMUM_WITHDRAWAL_BTC_AMOUNT") ?? throw new InvalidOperationException();
+        var minimum = decimal.Parse(environmentVariableMin, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
+        var maximum = decimal.Parse(environmentVariableMax, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
+        
         if (amount < minimum && !isAmountDisabled)
         {
             obj.Status = ValidationStatus.Error;
             obj.ErrorText = $"Error, the minimum amount to withdraw is at least {minimum} BTC";
+        }
+
+        if (amount > maximum && !isAmountDisabled)
+        {
+            obj.Status = ValidationStatus.Error;
+            obj.ErrorText = $"Error, the maximum amount to withdraw is {maximum} BTC";
         }
     }
 
@@ -100,20 +108,6 @@ public static class ValidationHelper
     {
         obj.Status = ValidationStatus.Success;
         if (destNode == null)
-        {
-            obj.ErrorText = "Select a proper destination node";
-            obj.Status = ValidationStatus.Error;
-        }
-    }
-
-    public static void validateMaxDecimal(ValidatorEventArgs obj)
-    {
-        obj.Status = ValidationStatus.Success;
-        
-        var environmentVariable = Environment.GetEnvironmentVariable("MAX_BTC") ?? throw new InvalidOperationException();
-        var max = decimal.Parse(environmentVariable, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
-        
-        if ((decimal)obj.Value > max)
         {
             obj.ErrorText = "Select a proper destination node";
             obj.Status = ValidationStatus.Error;
