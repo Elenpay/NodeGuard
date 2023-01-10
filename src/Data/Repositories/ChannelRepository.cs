76095c4ac87c12e570cae797ea138b511359817a
Rodrigo Sanchez <rodrigo@clovrlabs.com>
2023-01-10 12:39:02 +0100

    Fixed typo

--- src/Data/Repositories/ChannelRepository.cs
+++ src/Data/Repositories/ChannelRepository.cs
@@ -119,7 +119,7 @@ namespace FundsManager.Data.Repositories
             map.Put("closeRequestId", closeRequest.Id);
             map.Put("forceClose", forceClose);
 
-            var retryList = RetriableJob.ParseRetryListFromEnvironmenVariable("JOB_RETRY_INTEVAL_LIST_IN_MINUTES");
+            var retryList = RetriableJob.ParseRetryListFromEnvironmenVariable("JOB_RETRY_INTERVAL_LIST_IN_MINUTES");
             var job = RetriableJob.Create<ChannelCloseJob>(map, closeRequest.Id.ToString(), retryList);
             await scheduler.ScheduleJob(job.Job, job.Trigger);
 
