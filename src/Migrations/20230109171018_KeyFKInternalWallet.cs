99be5c9da5a0971e91827c22e1936a8806278045
José A.P <53834183+Jossec101@users.noreply.github.com>
2023-01-10 10:53:33 +0100

    Fixed #37 (#42)
    * Fixed #37
    
    Added new migration for adding the master fingerprint to a internal wallet during setup
    Refactored the views for displaying the fingerprint
    
    * Derivation path was not set
    
    * The internal wallet setup page will render again if the master fingerprint or the xpub is missing

---
+++ src/Migrations/20230109171018_KeyFKInternalWallet.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FundsManager.Migrations
+{
+    public partial class KeyFKInternalWallet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFundsManagerPrivateKey",
+                table: "Keys");
+
+            migrationBuilder.AddColumn<int>(
+                name: "InternalWalletId",
+                table: "Keys",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MasterFingerprint",
+                table: "InternalWallets",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Keys_InternalWalletId",
+                table: "Keys",
+                column: "InternalWalletId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Keys_InternalWallets_InternalWalletId",
+                table: "Keys",
+                column: "InternalWalletId",
+                principalTable: "InternalWallets",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Keys_InternalWallets_InternalWalletId",
+                table: "Keys");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Keys_InternalWalletId",
+                table: "Keys");
+
+            migrationBuilder.DropColumn(
+                name: "InternalWalletId",
+                table: "Keys");
+
+            migrationBuilder.DropColumn(
+                name: "MasterFingerprint",
+                table: "InternalWallets");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFundsManagerPrivateKey",
+                table: "Keys",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
