64d71c4d0fd47e98d0fceefabf45d406d67e34b3
Marcos <marcos@clovrlabs.com>
2023-01-12 15:03:32 +0100

    Change ClovrLabs -> Elenpay

--- src/Jobs/ProcessNodeChannelAcceptorJob.cs
+++ src/Jobs/ProcessNodeChannelAcceptorJob.cs
@@ -1,6 +1,6 @@
 /*
  * NodeGuard
- * Copyright (C) 2023  ClovrLabs
+ * Copyright (C) 2023  Elenpay
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -13,7 +13,7 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
  *
  */
 
