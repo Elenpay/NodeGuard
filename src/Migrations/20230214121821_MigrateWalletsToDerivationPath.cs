0aec36906930bf0fdf9d78ca5c22310710c83486
Rodrigo Sanchez <rodrigo@clovrlabs.com>
2023-02-14 13:34:52 +0100

    Added migration for existing wallets, added headers

--- src/Migrations/20230214121821_MigrateWalletsToDerivationPath.cs
+++ src/Migrations/20230214121821_MigrateWalletsToDerivationPath.cs
@@ -23,44 +23,25 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace FundsManager.Migrations
 {
-    public partial class ChannelNode : Migration
+    public partial class MigrateWalletsToDerivationPath : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<int>(
-                name: "NodeId",
-                table: "Channels",
-                type: "integer",
-                nullable: false,
-                defaultValue: 0);
-
             migrationBuilder.CreateIndex(
-                name: "IX_Channels_NodeId",
-                table: "Channels",
-                column: "NodeId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Channels_Nodes_NodeId",
-                table: "Channels",
-                column: "NodeId",
-                principalTable: "Nodes",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
+                name: "IX_Wallets_InternalWalletSubDerivationPath",
+                table: "Wallets",
+                column: "InternalWalletSubDerivationPath",
+                unique: true);
+            migrationBuilder.Sql("UPDATE \"InternalWallets\" SET \"DerivationPath\" = 'm/48''/1'''");
+            migrationBuilder.Sql("UPDATE \"Wallets\" SET \"InternalWalletSubDerivationPath\" = '1'");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Channels_Nodes_NodeId",
-                table: "Channels");
-
             migrationBuilder.DropIndex(
-                name: "IX_Channels_NodeId",
-                table: "Channels");
-
-            migrationBuilder.DropColumn(
-                name: "NodeId",
-                table: "Channels");
+                name: "IX_Wallets_InternalWalletSubDerivationPath",
+                table: "Wallets");
+            migrationBuilder.Sql("UPDATE \"InternalWallets\" SET \"DerivationPath\" = 'm/48''/1''/1'''");
         }
     }
 }
