b17ccc9dc38d6ccfc0a18afa8b013d1baa83a2de
Rodrigo Sanchez <rodrigo@clovrlabs.com>
2023-02-28 16:12:59 +0100

    Fixed non passing unit tests

--- test/FundsManager.Tests/Data/Models/WalletTests.cs
+++ test/FundsManager.Tests/Data/Models/WalletTests.cs
@@ -30,7 +30,7 @@ public class WalletTests
         // Assert
         var derivation = result!.GetDerivation();
         derivation.Redeem.Should().BeNull();
-        derivation.ScriptPubKey.ToString().Should().Be("0 7364aa1f2950a8ba6e3bc1a6f2c4d187975625a5");
+        derivation.ScriptPubKey.ToString().Should().Be("0 796cf3de2a828f0fa18f0f7d2cd11ea7273aca1b");
     }
 
     [Fact]
@@ -46,12 +46,12 @@ public class WalletTests
         derivation.Redeem.ToString().Should().Be(
             "2 " +
             "0251af15e05bb8b7eac1895b3f5f47ffefecd8321a02ef22298f89d0a2037df060 " +
-            "032223852338ea67fbfd8dc352ab2625e79a27764621236bf1c0213090d3489e9f " +
             "03808e2e1cd482bdd07196943c5a8133d169af86022b6bb0381771644ec07444d7 " +
+            "03afeb3ea5c2a70dc4aa2872964a5e2ff1172af3466a1bfed039b62665497fc546 " +
             "3 " +
             "OP_CHECKMULTISIG"
         );
 
-        derivation.ScriptPubKey.ToString().Should().Be("0 8a0a76a2e50c0233e857d0dbcb7a204e8747aa6c005e3bfbb5abb217f0f73783");
+        derivation.ScriptPubKey.ToString().Should().Be("0 467798178e61383c90c2b55b1b3255b703caf09d74df3990b0094690090b6ad0");
     }
 }
