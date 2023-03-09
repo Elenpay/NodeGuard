2e837146a9a344f17c050854ab4ebe8d20024893
José A.P <53834183+Jossec101@users.noreply.github.com>
2023-03-09 14:02:24 +0100

    Add SQL-based migration to fix SourceNodeId and DestinationNodeId fields from Channels by looking at its requests

---
+++ src/Migrations/20230309121807_UpdateSourceDestChannelNodeIdFromRequests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FundsManager.Migrations
+{
+    public partial class UpdateSourceDestChannelNodeIdFromRequests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string query = @"
+BEGIN TRANSACTION;
+
+UPDATE public.""Channels"" AS c
+SET
+    ""SourceNodeId"" = cor.""SourceNodeId"",
+    ""DestinationNodeId"" = cor.""DestNodeId""
+FROM (
+    SELECT
+        ""ChannelId"",
+        ""SourceNodeId"",
+        ""DestNodeId"",
+        ""RequestType""
+    FROM public.""ChannelOperationRequests""
+    WHERE ""SourceNodeId"" IS NOT NULL AND ""DestNodeId"" IS NOT NULL AND ""ChannelId"" IS NOT NULL AND ""RequestType"" = 1
+    ORDER BY ""CreationDatetime""
+) AS cor
+WHERE c.""Id"" = cor.""ChannelId"";
+
+COMMIT TRANSACTION;
+";
+
+            migrationBuilder.Sql(query);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string query = @"
+BEGIN TRANSACTION;
+    UPDATE public.""Channels"" SET ""SourceNodeId"" = 1, ""DestinationNodeId"" = 1;
+COMMIT;";
+            migrationBuilder.Sql(query);
+
+        }
+    }
+}
