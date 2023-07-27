dfc13e656b0b0f30f1596a98c4d120ba5930fb00
José A.P <53834183+Jossec101@users.noreply.github.com>
2023-07-27 16:05:19 +0200

    nullable status logs

---
+++ src/Migrations/20230725153505_NullableStatusLogs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using FundsManager.Helpers;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FundsManager.Migrations
+{
+    public partial class NullableStatusLogs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<ChannelStatusLog>>(
+                name: "StatusLogs",
+                table: "ChannelOperationRequests",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(List<ChannelStatusLog>),
+                oldType: "jsonb");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<ChannelStatusLog>>(
+                name: "StatusLogs",
+                table: "ChannelOperationRequests",
+                type: "jsonb",
+                nullable: false,
+                oldClrType: typeof(List<ChannelStatusLog>),
+                oldType: "jsonb",
+                oldNullable: true);
+        }
+    }
+}
