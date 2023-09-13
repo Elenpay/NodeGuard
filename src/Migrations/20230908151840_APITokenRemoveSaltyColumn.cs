cf3b4a7b8c9e09498a53994503b7b25384359a99
daliclovr <dalila@clovrlabs.com>
2023-09-13 18:20:17 +0200

    added migration files

---
+++ src/Migrations/20230908151840_APITokenRemoveSaltyColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NodeGuard.Migrations
+{
+    /// <inheritdoc />
+    public partial class APITokenRemoveSaltyColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSalty",
+                table: "ApiTokens");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSalty",
+                table: "ApiTokens",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
