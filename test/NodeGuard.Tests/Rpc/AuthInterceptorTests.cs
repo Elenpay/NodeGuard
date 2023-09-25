1ae1c81ca31f24d1a4b759f238a4bbf1758d29d8
daliclovr <dalila@clovrlabs.com>
2023-09-25 17:09:49 +0200

    commented different part of tests

--- test/NodeGuard.Tests/Rpc/AuthInterceptorTests.cs
+++ test/NodeGuard.Tests/Rpc/AuthInterceptorTests.cs
@@ -11,6 +11,7 @@ public class AuthInterceptorTests
     [Fact]
     public async Task AuthInterceptor_NoTokenProvided()
     {
+        // Arrange
         var context = TestServerCallContext.Create();
         var mockedApiTokenRepository = new Mock<IAPITokenRepository>();
         var interceptor = new ServerAuthInterceptor(mockedApiTokenRepository.Object);
@@ -18,6 +19,7 @@ public class AuthInterceptorTests
 
         var expectedMessage = "No token provided";
         
+        // Act & Assert
         var exception = await Assert.ThrowsAsync<RpcException>(() => interceptor.UnaryServerHandler<string, string>(String.Empty, context, continuation));
         Assert.Equal(expectedMessage, exception.Status.Detail);
     }
@@ -25,6 +27,7 @@ public class AuthInterceptorTests
     [Fact]
     public async Task AuthInterceptor_NonExistingToken()
     {
+        // Arrange
         var context = TestServerCallContext.Create(new Metadata{{"auth-token", "iamastupidtoken"}});
         var mockedApiTokenRepository = new Mock<IAPITokenRepository>();
         var interceptor = new ServerAuthInterceptor(mockedApiTokenRepository.Object);
@@ -32,6 +35,7 @@ public class AuthInterceptorTests
 
         var expectedMessage = "Invalid token";
         
+        // Act & Assert
         var exception = await Assert.ThrowsAsync<RpcException>(() => interceptor.UnaryServerHandler<string, string>(String.Empty, context, continuation));
         Assert.Equal(expectedMessage, exception.Status.Detail);
     }
@@ -39,6 +43,7 @@ public class AuthInterceptorTests
     [Fact]
     public async Task AuthInterceptor_ExistingTokenValid()
     {
+        // Arrange
         var validToken = "iamavalidtoken";
         var apiTokenFixture = new APIToken { TokenHash = validToken };
         var mockedApiTokenRepository = new Mock<IAPITokenRepository>();
@@ -57,8 +62,11 @@ public class AuthInterceptorTests
         
         // In order to test if the request is continued, which would mean that the token is valid,
         // we mock the continuation function and we verify that it is called once.
-
+        
+        // Act
         await interceptor.UnaryServerHandler<string, string>(String.Empty, context, mockContinuation.Object);
+        
+        // Assert
         mockContinuation.Verify(
             c => c(
                 It.IsAny<string>(),
@@ -69,6 +77,7 @@ public class AuthInterceptorTests
     [Fact]
     public async Task AuthInterceptor_ExistingTokenInvalid()
     {
+        // Arrange
         var validToken = "iamaninvalidtoken";
         var apiTokenFixture = new APIToken { TokenHash = validToken , IsBlocked = true};
         var mockedApiTokenRepository = new Mock<IAPITokenRepository>();
@@ -79,10 +88,10 @@ public class AuthInterceptorTests
         var context = TestServerCallContext.Create(new Metadata{{"auth-token", validToken}});
         var interceptor = new ServerAuthInterceptor(mockedApiTokenRepository.Object);
         var continuation = new UnaryServerMethod<string, string>(async (request, context) => { return "response"; });
-
         
         var expectedMessage = "Invalid token";
 
+        // Act & Assert
         var exception = await Assert.ThrowsAsync<RpcException>(() => interceptor.UnaryServerHandler<string, string>(String.Empty, context, continuation));
         Assert.Equal(expectedMessage, exception.Status.Detail);
     }
