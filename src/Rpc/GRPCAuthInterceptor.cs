9464406c85b882a89bb3c54b67a2dcbf8861fa65
daliclovr <dalila@clovrlabs.com>
2023-10-13 18:28:41 +0200

    added feature flag to enable authentication

--- src/Rpc/GRPCAuthInterceptor.cs
+++ src/Rpc/GRPCAuthInterceptor.cs
@@ -18,17 +18,20 @@ public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(
     ServerCallContext context, 
     UnaryServerMethod<TRequest, TResponse> continuation)
     {
-        var token = context.RequestHeaders.FirstOrDefault(x => x.Key == "auth-token")?.Value;
-        if (token == null)
-        {
-            throw new RpcException(new Status(StatusCode.Unauthenticated, "No token provided"));
-        }
-
-        var apiToken = await _apiTokenRepository.GetByToken(token);
         
-        if (apiToken?.IsBlocked ?? true)
-        {
-            throw new RpcException(new Status(StatusCode.Unauthenticated, "Invalid token"));
+        if (Constants.GRPC_AUTH_FEATURE_FLAG) {
+            var token = context.RequestHeaders.FirstOrDefault(x => x.Key == "auth-token")?.Value;
+            if (token == null)
+            {
+                throw new RpcException(new Status(StatusCode.Unauthenticated, "No token provided"));
+            }
+
+            var apiToken = await _apiTokenRepository.GetByToken(token);
+            
+            if (apiToken?.IsBlocked ?? true)
+            {
+                throw new RpcException(new Status(StatusCode.Unauthenticated, "Invalid token"));
+            }
         }
         
         return await continuation(request, context);
