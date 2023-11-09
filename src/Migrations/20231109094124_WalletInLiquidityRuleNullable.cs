36167a7d249a6947de61f0efd6d35571add94c6d
Marcos <marferp2@inf.upv.es>
2023-11-09 10:46:33 +0100

    feat: wallet in liquidity rule nullable

---
+++ src/Migrations/20231109094124_WalletInLiquidityRuleNullable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NodeGuard.Migrations
+{
+    /// <inheritdoc />
+    public partial class WalletInLiquidityRuleNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LiquidityRules_Wallets_WalletId",
+                table: "LiquidityRules");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WalletId",
+                table: "LiquidityRules",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LiquidityRules_Wallets_WalletId",
+                table: "LiquidityRules",
+                column: "WalletId",
+                principalTable: "Wallets",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LiquidityRules_Wallets_WalletId",
+                table: "LiquidityRules");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WalletId",
+                table: "LiquidityRules",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LiquidityRules_Wallets_WalletId",
+                table: "LiquidityRules",
+                column: "WalletId",
+                principalTable: "Wallets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
