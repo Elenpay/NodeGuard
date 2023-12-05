9cdbc6ecebf162756ce5e15541ed6c24979ad546
oleksandrK <alex.karpenko@clovrlabs.com>
2023-12-05 16:23:34 +0100

    Added migrations files.

---
+++ src/Migrations/20231205152120_DisableNode.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NodeGuard.Migrations
+{
+    /// <inheritdoc />
+    public partial class DisableNode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isEnabledNode",
+                table: "Nodes",
+                newName: "IsNodeEnabled");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsNodeEnabled",
+                table: "Nodes",
+                newName: "isEnabledNode");
+        }
+    }
+}
