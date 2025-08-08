4e41a9d53fd4e542d4edbf8321bdf0b534db9771
Marcos <33052423+markettes@users.noreply.github.com>
2025-08-08 15:54:27 +0200

    [GEN-1670] Add SwapOut model (#422)
    * fix: connection problems, healthcheck postgres
    
    * feat: add swapouts to database
    
    * fix: remove dbtrie
    
    * fix: small fixes (thanks copilot)
    
    * fix: remove unnecessary repo method
    
    * fix: not nullable
    
    * fix: change var name
    
    * fix: remove unused method
    
    * fix: remove unused attributes

---
+++ src/Data/Repositories/SwapOutRepository.cs
@@ -0,0 +1,107 @@
+/*
+ * NodeGuard
+ * Copyright (C) 2023  Elenpay
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NodeGuard.Data.Models;
+using NodeGuard.Data.Repositories.Interfaces;
+
+namespace NodeGuard.Data.Repositories
+{
+   public class SwapOutRepository : ISwapOutRepository
+   {
+      private readonly IRepository<SwapOut> _repository;
+      private readonly IDbContextFactory<ApplicationDbContext> _dbContextFactory;
+
+      public SwapOutRepository(IRepository<SwapOut> repository, IDbContextFactory<ApplicationDbContext> dbContextFactory)
+      {
+         _repository = repository;
+         _dbContextFactory = dbContextFactory;
+      }
+
+      public async Task<SwapOut?> GetById(int id)
+      {
+         await using var context = await _dbContextFactory.CreateDbContextAsync();
+
+         var swap = await context.SwapOuts
+            .Include(s => s.DestinationWallet)
+            .Include(s => s.UserRequestor)
+            .SingleOrDefaultAsync(s => s.Id == id);
+
+         return swap;
+      }
+
+      public async Task<List<SwapOut>> GetByIds(List<int> ids)
+      {
+         await using var context = await _dbContextFactory.CreateDbContextAsync();
+
+         var swaps = await context.SwapOuts
+            .Include(s => s.DestinationWallet)
+            .Include(s => s.UserRequestor)
+            .Where(s => ids.Contains(s.Id))
+            .ToListAsync();
+
+         return swaps;
+      }
+
+      public async Task<List<SwapOut>> GetAll()
+      {
+         await using var context = await _dbContextFactory.CreateDbContextAsync();
+
+         var swaps = await context.SwapOuts
+            .Include(s => s.DestinationWallet)
+            .Include(s => s.UserRequestor)
+            .ToListAsync();
+
+         return swaps;
+      }
+
+      public async Task<(bool, string?)> AddAsync(SwapOut swap)
+      {
+         await using var context = await _dbContextFactory.CreateDbContextAsync();
+
+         swap.SetCreationDatetime();
+         swap.SetUpdateDatetime();
+
+         return await _repository.AddAsync(swap, context);
+      }
+
+      public async Task<(bool, string?)> AddRangeAsync(List<SwapOut> swaps)
+      {
+         await using var context = await _dbContextFactory.CreateDbContextAsync();
+
+         foreach (var swap in swaps)
+         {
+            swap.SetCreationDatetime();
+            swap.SetUpdateDatetime();
+         }
+
+         return await _repository.AddRangeAsync(swaps, context);
+      }
+
+      public (bool, string?) Update(SwapOut swap)
+      {
+         using var context = _dbContextFactory.CreateDbContext();
+
+         swap.SetUpdateDatetime();
+
+         return _repository.Update(swap, context);
+      }
+   }
+}
---
+++ src/Migrations/20250805114247_AddSwapOuts.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace NodeGuard.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSwapOuts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SwapOuts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Provider = table.Column<int>(type: "integer", nullable: false),
+                    ProviderId = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    IsManual = table.Column<bool>(type: "boolean", nullable: false),
+                    SatsAmount = table.Column<long>(type: "bigint", nullable: false),
+                    DestinationWalletId = table.Column<int>(type: "integer", nullable: true),
+                    ServiceFeeSats = table.Column<long>(type: "bigint", nullable: true),
+                    LightningFeeSats = table.Column<long>(type: "bigint", nullable: true),
+                    OnChainFeeSats = table.Column<long>(type: "bigint", nullable: true),
+                    ErrorDetails = table.Column<string>(type: "text", nullable: true),
+                    UserRequestorId = table.Column<string>(type: "text", nullable: true),
+                    TxId = table.Column<string>(type: "text", nullable: true),
+                    CreationDatetime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdateDatetime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SwapOuts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SwapOuts_AspNetUsers_UserRequestorId",
+                        column: x => x.UserRequestorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_SwapOuts_Wallets_DestinationWalletId",
+                        column: x => x.DestinationWalletId,
+                        principalTable: "Wallets",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SwapOuts_DestinationWalletId",
+                table: "SwapOuts",
+                column: "DestinationWalletId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SwapOuts_UserRequestorId",
+                table: "SwapOuts",
+                column: "UserRequestorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SwapOuts");
+        }
+    }
+}
