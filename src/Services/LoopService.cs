cf8067e3c9ee0bf94908b129625dd47f5d1d6335
Rodrigo <39995243+RodriFS@users.noreply.github.com>
2025-09-12 10:44:33 +0200

    remove hardcoded MaxPrepayAmt

--- src/Services/LoopService.cs
+++ src/Services/LoopService.cs
@@ -105,7 +105,6 @@ public class LoopService : ILoopService
         {
             Amt = request.Amount,
             Dest = request.Address,
-            MaxPrepayAmt = 5000, // Satoshis
         };
 
         if (request.MaxFees.HasValue)
