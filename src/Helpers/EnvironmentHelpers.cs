20ead2404b7cc8475b45c38bb713556a64b37235
Rodrigo <39995243+RodriFS@users.noreply.github.com>
2025-10-01 13:40:16 +0200

    Throw if EnvironmentHelpers.GetOrDefault fails to cast

--- src/Helpers/EnvironmentHelpers.cs
+++ src/Helpers/EnvironmentHelpers.cs
@@ -2,20 +2,24 @@ namespace NodeGuard.Helpers
 {
     public static class EnvironmentHelpers
     {
+        /// <summary>
+        /// Gets the value of an environment variable and converts it to the specified type.
+        /// If the environment variable is not set or is empty, returns the provided default value.
+        /// </summary>
+        /// <typeparam name="T">The type to convert the environment variable value to.</typeparam>
+        /// <param name="variableName">The name of the environment variable.</param>
+        /// <param name="defaultValue">The default value to return if the environment variable is not set or is empty.</param>
+        /// <returns>The value of the environment variable converted to type T, or the default value.</returns>
+        /// <exception cref="InvalidCastException">Thrown if the environment variable value cannot be converted to type T.</exception>
+        /// <exception cref="FormatException">Thrown if the environment variable value is not in a format recognized by type T.</exception>
+        /// <exception cref="OverflowException">Thrown if the environment variable value represents a number less than MinValue or greater than MaxValue of type T. (If applicable)</exception>
         public static T GetOrDefault<T>(string variableName, T defaultValue)
         {
-            var value = System.Environment.GetEnvironmentVariable(variableName);
+            var value = Environment.GetEnvironmentVariable(variableName);
             if (string.IsNullOrEmpty(value))
                 return defaultValue;
 
-            try
-            {
-                return (T)Convert.ChangeType(value, typeof(T));
-            }
-            catch
-            {
-                return defaultValue;
-            }
+            return (T)Convert.ChangeType(value, typeof(T));
         }
     }
 }
