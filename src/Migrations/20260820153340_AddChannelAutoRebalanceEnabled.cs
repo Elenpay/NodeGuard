392176839108d3734de4b890cfe9ee97c18df97f
Marcos <33052423+markettes@users.noreply.github.com>
2026-09-01 01:37:44 +0900

    feat: add per-channel automated rebalancing opt-in (#581)

---
+++ src/Migrations/20260820153340_AddChannelAutoRebalanceEnabled.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NodeGuard.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChannelAutoRebalanceEnabled : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAutoRebalanceEnabled",
+                table: "Channels",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAutoRebalanceEnabled",
+                table: "Channels");
+        }
+    }
+}
